dd03f7116df469414c87d5de9fb49a20963bcbfd
Musawer49 <musawerahmedkhan2003@gmail.com>
2026-08-09 23:48:23 +0500

    Added IF, ELIF and Else

--- unit_converter.py
+++ unit_converter.py
@@ -1,7 +1,13 @@
 print("Welcome to Unit Converter!")
-
 print("\n1. Length")
 print("2. Weight")
 print("3. Temperature")
-
-choice = input("\nEnter your choice: ")
+choice = int(input("\nEnter your choice: "))
+if choice == 1:
+    print("You selected Length")
+elif choice == 2 :
+    print("You selected Weight")
+elif choice == 3 :
+    print("You selected Temperature")
+else:
+    print("The given number is not valid.")

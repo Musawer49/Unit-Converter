4c45bf0ec5762812f7c79341b73eb43b49ddbecc
Musawer49 <musawerahmedkhan2003@gmail.com>
2026-08-10 23:00:53 +0500

    Added some features

--- unit_converter.py
+++ unit_converter.py
@@ -5,6 +5,13 @@ print("3. Temperature")
 choice = int(input("\nEnter your choice: "))
 if choice == 1:
     print("You selected Length")
+    choose = input("Choose an option: (A) Miles or (B) Kilometers: ").strip().upper()
+    if choose == "A": 
+        print("You have selected Miles")
+        ask_user = int(input("Enter the number of Miles: "))
+        Kilometer = ask_user * 1.609
+        print(f"Your answer is {Kilometer} kilometers")
+
 elif choice == 2 :
     print("You selected Weight")
 elif choice == 3 :

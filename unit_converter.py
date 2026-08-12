84a40feefd3b48c7cc41d21250c0b51950b63b9f
Musawer49 <musawerahmedkhan2003@gmail.com>
2026-08-12 23:12:10 +0500

    Tried try exception error handling concept

--- unit_converter.py
+++ unit_converter.py
@@ -8,12 +8,15 @@ if choice == 1:
     choose = input("Choose an option: (A) Miles or (B) Kilometers: ").strip().upper()
     if choose == "A": 
         print("You have selected Miles")
-        ask_user = int(input("Enter the Length in Miles: "))
-        Kilometer = ask_user * 1.609
-        print(f"Your Length is {Kilometer} kilometers")
+        try:
+            ask_user = float(input("Enter the Length in Miles: "))
+            Kilometer = ask_user * 1.609
+            print(f"Your Length is {Kilometer} kilometers")
+        except ValueError:
+            print("You have entered invalid number. Please enter a valid number")
     elif choose == "B":
         print("You have selected Kilometers")
-        ask_user = int(input("Enter the Length in Kilometers: "))
+        ask_user = float(input("Enter the Length in Kilometers: "))
         Mile = ask_user / 1.609
         print(f"The Length is {Mile} Miles.")
     else:
@@ -23,12 +26,12 @@ elif choice == 2 :
     choose = input("Choose an option: (A) Kilograms or (B) Pounds: ").strip().upper()
     if choose == "A":
         print("You have selected Kilograms")
-        ask_user = int(input("Enter the weight in Kilograms: "))
+        ask_user = float(input("Enter the weight in Kilograms: "))
         Pound = ask_user * 2.2046
         print(f"The weight is {Pound} Pounds")
     elif choose == "B":
         print("You have selected Pounds")
-        ask_user = int(input("Enter the weight in Pounds: "))
+        ask_user = float(input("Enter the weight in Pounds: "))
         Kilogram = ask_user * 0.4536
         print(f"The weight is {Kilogram} Kilograms")
     else:
@@ -38,12 +41,12 @@ elif choice == 3 :
     choose = input("Choose an option: (A) Fahrenheit or (B) Celsius: ").strip().upper()
     if choose == "A":
         print("You have selected Fahrenheit")
-        ask_user = int(input("Enter the temperature in Fahrenheit: "))
+        ask_user = float(input("Enter the temperature in Fahrenheit: "))
         Celsius = (ask_user - 32) / 1.8
         print(f"The temperature is {Celsius} Celsius")
     elif choose == "B":
         print("You have selected Celsius")
-        ask_user = int(input("Enter the temperature in Celsius: "))
+        ask_user = float(input("Enter the temperature in Celsius: "))
         Fahrenheit = (ask_user * 1.8) + 32
         print(f"The temperature is {Fahrenheit} Fahrenheit")
     else:
